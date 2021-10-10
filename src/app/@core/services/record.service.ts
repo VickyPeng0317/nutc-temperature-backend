964a43f4888ffb5119c87cc7e90016ded1016b51
peng_xiang <cxz0917001997@gmail.com>
2021-10-10 21:32:33 +0800

    feat 辨識紀錄 - 界接 API

--- src/app/@core/services/record.service.ts
+++ src/app/@core/services/record.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
+import { environment } from '@environments/environment';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,18 +12,20 @@ export class RecordService {
   ) { }
 
   getRecordList() {
-    const recordInfo: IRecordInfo = {
-      id:1,
-      userId: 1,
-      userName: '王大明',
-      userAccount: '1710932013',
-      deviceId: 1,
-      deviceName: '體溫裝置01',
-      temperature: '36.5',
-      createdTime: '2021-10-01 08:00:00',
-    };
-    const res = [1,1,1,1,1,1].map(() => recordInfo);
-    return of(res);
+    const url = environment.apiUrl + '/record/list';
+    return this.http.get<IRecordInfo[]>(url);
+    // const recordInfo: IRecordInfo = {
+    //   id:1,
+    //   userId: 1,
+    //   userName: '王大明',
+    //   userAccount: '1710932013',
+    //   deviceId: 1,
+    //   deviceName: '體溫裝置01',
+    //   temperature: '36.5',
+    //   createdTime: '2021-10-01 08:00:00',
+    // };
+    // const res = [1,1,1,1,1,1].map(() => recordInfo);
+    // return of(res);
   }
 
   getRecordInfo(id: number) {
