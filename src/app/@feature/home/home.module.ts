f7314aad3dd3d00e06676706d3b4ba93f49a5718
peng_xiang <cxz0917001997@gmail.com>
2021-10-19 21:43:23 +0800

    remove unuse component

--- src/app/@feature/home/home.module.ts
+++ src/app/@feature/home/home.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { CoreModule } from '@core/core.module';
-import { ChartHumidityComponent } from './components/chart-humidity/chart-humidity.component';
-import { ChartTemperatureComponent } from './components/chart-temperature/chart-temperature.component';
-import { ChartWaterCountComponent } from './components/chart-water-count/chart-water-count.component';
-import { ChartTemperatureAndHumidityComponent } from './components/chart-temperature-and-humidity/chart-temperature-and-humidity.component';
 import { HomeChartDaysComponent } from './components/home-chart-days/home-chart-days.component';
 import { HomeChartHoursComponent } from './components/home-chart-hours/home-chart-hours.component';
 import { HomeUserListComponent } from './components/home-user-list/home-user-list.component';
 import { HomeUserPercentComponent } from './components/home-user-percent/home-user-percent.component';
 
 @NgModule({
-  declarations: [HomeComponent, ChartHumidityComponent, ChartTemperatureComponent, ChartWaterCountComponent, ChartTemperatureAndHumidityComponent, HomeChartDaysComponent, HomeChartHoursComponent, HomeUserListComponent, HomeUserPercentComponent],
+  declarations: [HomeComponent, HomeChartDaysComponent, HomeChartHoursComponent, HomeUserListComponent, HomeUserPercentComponent],
   imports: [
     CommonModule,
     SharedModule,
