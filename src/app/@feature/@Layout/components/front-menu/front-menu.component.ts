8bcfe6dad02f8163ff63102bc40810d4eee1c0fc
peng_xiang <cxz0917001997@gmail.com>
2021-10-25 21:14:09 +0800

    feat 選單 - 權限控管

--- src/app/@feature/@Layout/components/front-menu/front-menu.component.ts
+++ src/app/@feature/@Layout/components/front-menu/front-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from '@core/services/store.service';
 import { NbMenuItem } from '@nebular/theme';
 
 @Component({
@@ -7,26 +8,30 @@ import { NbMenuItem } from '@nebular/theme';
   styleUrls: ['./front-menu.component.scss']
 })
 export class FrontMenuComponent implements OnInit {
-  items: NbMenuItem[] = [
+  items: IMenuItem[] = [
     {
       title: '首頁',
       icon: 'home',
-      link: 'home'
+      link: 'home',
+      identity: 'teacher'
     },
     {
       title: '大門設備',
       icon: 'inbox',
-      link: '/front/home/door'
+      link: '/front/home/door',
+      identity: 'teacher'
     },
     {
       title: '裝置管理',
       icon: 'speaker',
-      link: 'device'
+      link: 'device',
+      identity: 'teacher'
     },
     {
       title: '使用者管理',
       icon: 'person',
-      link: 'user'
+      link: 'user',
+      identity: 'teacher'
     },
     {
       title: '辨識記錄查詢',
@@ -34,9 +39,23 @@ export class FrontMenuComponent implements OnInit {
       link: 'record'
     }
   ];
-  constructor() { }
+  constructor(
+    private storeService: StoreService
+  ) { }
 
   ngOnInit(): void {
+    const userIdentity = this.storeService.getUserInfo().identity;
+    this.items = this.items.filter(x => {
+      const { identity } = x;
+      const isShow = !!identity
+        ? userIdentity === identity
+        : true;
+      return isShow;
+    });
   }
 
+}
+
+interface IMenuItem extends NbMenuItem {
+  identity?: string;
 }
